caa6605cf75df41b68a7c387ad94a675662e54d7
Ylva Sjölin <ylva@spektatum.com>
2019-04-24 05:42:16 +0100

    Updates done 0.2v

---
+++ config/router/test.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Load the stylechooser as a controller class.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Testin functionalities.",
+            "mount" => "test2",
+            "handler" => "",
+        ],
+    ]
+];
